4f0e83bd2f72dcc0deef326dd87c69eb94ee9838
Ryan Iki <ryan.iki@duke.edu>
2021-04-26 11:34:41 -0400

    Update part of detectArbitrage for first V-1 iterations

--- project3.py
+++ project3.py
@@ -18,16 +18,32 @@ from p3tests import *
 detectArbitrage
 """
 def detectArbitrage(adjList, adjMat, tol=1e-15):
-	adjList[0].dist = 0
+    # TODO: check how to choose start
+    # Select start vertex as first in adjList
+    start = adjList[0]
 
+    # Set initial dist for start as 0
+    start.dist = 0
+
+    # Create variable numV to represent number of vertices in graph
 	numV = len(adjList)
+
+    # Iterate numV - 1 times
 	for n in range(numV - 1):
+        # Look at each vertex in graph
         for vertex in adjList:
+            # Check each neighbor of vertex
+            # Update predictions and previous vertex
             for nbr in vertex.neigh:
-                if nbr.dist > vertex.dist + adjMat[vertex.rank][nbr.rank] + tol:
-                    nbr.dist = vertex.dist + adjMat[vertex.rank][nbr.rank]
+                # Create variable length to represent edge weight
+                length = adjMat[vertex.rank][nbr.rank]
+                # Only update if new value is better than tolerance value
+                if nbr.dist > vertex.dist + length + tol:
+                    nbr.dist = vertex.dist + length
                     nbr.prev = vertex
     loop = []
+    
+    # Iterate 1 more time to check for changes indicating arbitrage
     for vertex in adjList:
         for nbr in vertex.neigh:
             if nbr.dist > vertex.dist + adjMat[vertex.rank][nbr.rank] + tol:
